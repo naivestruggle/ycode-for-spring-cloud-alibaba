852ed0f2fe2e97ac7ec55ff036b6ccf125ac2f07
yangxinhu <2550438618@qq.com>
2020-02-08 11:47:57 +0800

    更新整个项目的说明文件

---
+++ add.bat
@@ -0,0 +1,118 @@
+git rm --cached ycode-server/cup-ycode/api/ycode-api-datetime
+git add ycode-server/cup-ycode/api/ycode-api-datetime/.
+
+git rm --cached ycode-server/cup-ycode/api/ycode-api-fastdfs
+git add ycode-server/cup-ycode/api/ycode-api-fastdfs/.
+
+git rm --cached ycode-server/cup-ycode/api/ycode-api-redis
+git add ycode-server/cup-ycode/api/ycode-api-redis/.
+
+git rm --cached ycode-server/cup-ycode/api/ycode-api-slideshow-admin
+git add ycode-server/cup-ycode/api/ycode-api-slideshow-admin/.
+
+git rm --cached ycode-server/cup-ycode/api/ycode-api-slideshow-user
+git add ycode-server/cup-ycode/api/ycode-api-slideshow-user/.
+
+git rm --cached ycode-server/cup-ycode/api/ycode-api-sso-admin
+git add ycode-server/cup-ycode/api/ycode-api-sso-admin/.
+
+git rm --cached ycode-server/cup-ycode/api/ycode-api-user-regist
+git add ycode-server/cup-ycode/api/ycode-api-user-regist/.
+
+
+
+
+
+
+
+
+git rm --cached ycode-server/cup-ycode/common/ycode-common-email
+git add ycode-server/cup-ycode/common/ycode-common-email/.
+
+git rm --cached ycode-server/cup-ycode/common/ycode-common-redis
+git add ycode-server/cup-ycode/common/ycode-common-redis/.
+
+git rm --cached ycode-server/cup-ycode/common/ycode-common-datetime
+git add ycode-server/cup-ycode/common/ycode-common-datetime/.
+
+git rm --cached ycode-server/cup-ycode/common/ycode-common-fastdfs
+git add ycode-server/cup-ycode/common/ycode-common-fastdfs/.
+
+git rm --cached ycode-server/cup-ycode/common/ycode-common-gateway
+git add ycode-server/cup-ycode/common/ycode-common-gateway/.
+
+
+
+
+
+
+
+
+
+git rm --cached ycode-server/cup-ycode/commons/ycode-commons
+git add ycode-server/cup-ycode/commons/ycode-commons/.
+
+git rm --cached ycode-server/cup-ycode/commons/ycode-commons-domain
+git add ycode-server/cup-ycode/commons/ycode-commons-domain/.
+
+git rm --cached ycode-server/cup-ycode/commons/ycode-commons-mapper
+git add ycode-server/cup-ycode/commons/ycode-commons-mapper/.
+
+git rm --cached ycode-server/cup-ycode/commons/ycode-commons-service
+git add ycode-server/cup-ycode/commons/ycode-commons-service/.
+
+git rm --cached ycode-server/cup-ycode/commons/ycode-commons-web
+git add ycode-server/cup-ycode/commons/ycode-commons-web/.
+
+
+
+
+
+
+
+
+
+git rm --cached ycode-server/cup-ycode/service/ycode-service-slideshow-admin
+git add ycode-server/cup-ycode/service/ycode-service-slideshow-admin/.
+
+git rm --cached ycode-server/cup-ycode/service/ycode-service-slideshow-user
+git add ycode-server/cup-ycode/service/ycode-service-slideshow-user/.
+
+git rm --cached ycode-server/cup-ycode/service/ycode-service-sso-admin
+git add ycode-server/cup-ycode/service/ycode-service-sso-admin/.
+
+git rm --cached ycode-server/cup-ycode/service/ycode-service-user-regist
+git add ycode-server/cup-ycode/service/ycode-service-user-regist/.
+
+
+
+
+
+
+
+git rm --cached ycode-server/cup-ycode/web/ycode-web-slideshow-admin
+git add ycode-server/cup-ycode/web/ycode-web-slideshow-admin/.
+
+git rm --cached ycode-server/cup-ycode/web/ycode-web-slideshow-user
+git add ycode-server/cup-ycode/web/ycode-web-slideshow-user/.
+
+git rm --cached ycode-server/cup-ycode/web/ycode-web-sso-admin
+git add ycode-server/cup-ycode/web/ycode-web-sso-admin/.
+
+git rm --cached ycode-server/cup-ycode/web/ycode-web-user-regist
+git add ycode-server/cup-ycode/web/ycode-web-user-regist/.
+
+
+
+
+
+
+
+git rm --cached ycode-server/cup-ycode/ycode-database
+git add ycode-server/cup-ycode/ycode-database/.
+
+git rm --cached ycode-server/cup-ycode/ycode-dependencies
+git add ycode-server/cup-ycode/ycode-dependencies/.
+
+git rm --cached ycode-server/cup-ycode/ycode-external-skywalking
+git add ycode-server/cup-ycode/ycode-external-skywalking/.
